a7c3bc7f06247afcb94bbdcc752ab5a9b82e8177
Stuart Urback <stuart@urback.net>
2019-05-06 23:16:38 -0600

    Heh woops

--- src/Graph/apolloClient.js
+++ src/Graph/apolloClient.js
@@ -1,8 +1,8 @@
 import ApolloClient from "apollo-boost";
 
 const client = new ApolloClient({
-  // uri: "http://cipher-api.playthistonight.com",
-  uri: "http://localhost:4000"
+  uri: "http://cipher-api.playthistonight.com",
+  // uri: "http://localhost:4000"
 });
 
 const stripTypeName = ({ __typename, ...rest }) => {
