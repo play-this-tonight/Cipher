a9054eedde2e8cad6ddd6591c88f7a3a6ad64d3d
Stuart Urback <stuart@urback.net>
2019-05-08 22:39:59 -0600

    Clean up sorting to bump undefineds to the back

--- src/Components/Game/ClueGroups/sortGuesses.js
+++ src/Components/Game/ClueGroups/sortGuesses.js
@@ -2,6 +2,8 @@ const sortGuesses = (guesses, parentClueIndex) => {
   const correctAnswers = guesses.filter(({ parentConceptId }) => parentConceptId && parentClueIndex === parentConceptId);
   const incorrectAnswers = guesses.filter(({ isCorrect, guess }) => !isCorrect && guess === parentClueIndex);
 
+  incorrectAnswers.sort(({ parentConceptId: aParent }, { parentConceptId: bParent }) => ((aParent || 100) - (bParent || 100)));
+  // groups like answers together for people abd shoves undefineds to the back.
 
   return {
     correctAnswers,
